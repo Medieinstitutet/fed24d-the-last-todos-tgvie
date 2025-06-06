7272faec7062e9f8878942ae4fe0a563b425970e
tgvie <166875380+tgvie@users.noreply.github.com>
2025-06-06 05:50:40 +0200

    refactor todos array to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import { useState } from 'react';
 
 function App() {
-	const [todos, setTodos] = useState([
-		{
-			id: 1,
-			text: '✅ Click on a task to mark it as done',
-			done: false
-		},
-		{
-			id: 2,
-			text: '➕ Use the form below to add your own tasks',
-			done: false
-		},
-		{
-			id: 3,
-			text: '♻️ Click on a completed task to undo it',
-			done: false
-		},
-		{
-			id: 4,
-			text: `💾 Your todos are saved in the browser automatically, try to restart this page`,
-			done: false
-		},
-		{
-			id: 5,
-			text: '⬆️ Tasks are sorted to keep unfinished ones at the top',
-			done: false
-		}
+	const [todos, setTodos] = useState<Todo[]>([
+		new Todo(1, '✅ Click on a task to mark it as done'),
+		new Todo(2, '♻️ Click on a completed task to undo it'),
+		new Todo(3, '➕ Use the form below to add your own tasks'),
+		new Todo(4, `💾 Your todos are saved in the browser automatically, try to restart this page`),
+		new Todo(5, '⬆️ Tasks are sorted to keep unfinished ones at the top')
 	]);
+
+	const toggleTodo = (id: number) => {
+		setTodos(prev =>
+			prev.map(todo => {
+				if (todo.id === id) {
+					const updated = new Todo(todo.id, todo.text, !todo.done);
+					return updated;
+				}
+				return todo;
+			})
+		);
+	};
 
 	return (
 		<div>
